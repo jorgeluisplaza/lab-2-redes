faeba1039c2318338207b6805a9afaa9e8a8f9ae
Jorge <jorge.plaza@usach.cl>
2019-04-27 18:49:52 -0400

    Se hace low pass

--- lab2redes.py
+++ lab2redes.py
@@ -18,36 +18,66 @@ import math
 # Se genera los valores de tiempo entre cada amplitud
 audioTime = np.linspace(0, len(data)/fs, num=len(data))
 
+# Se grafica dominio del tiempo
+plotFunction.figure('Dominio del tiempo Audio Original')
+plotFunction.title('Dominio del tiempo Audio Original')
+plotFunction.plot(audioTime, data)
+
+# Se genera espectrograma del audio original
 (frequencies, times, audioSpectogram) = signal.spectrogram(data, fs)
 
+# Se genera el grafico
+plotFunction.figure('Espectograma')
 plotFunction.pcolormesh(times, frequencies, np.log10(audioSpectogram))
+plotFunction.title("Espectrograma del audio original")
 plotFunction.xlabel('Tiempo (S)')
 plotFunction.ylabel('Frecuencia (Hz)')
 plotFunction.colorbar()
 
+# Se calcula al transformada de Fourier
 fourierTransform = fourier(data)
 
-freqs = frequency(len(data), 1.0/fs)
+# Se obtienen las frecuencias
+freqs = frequency(len(data), 1/fs)
 
-cutOffFreq = 400.0
-freqRatio = (cutOffFreq/fs)
-N = int(math.sqrt(0.196196 + freqRatio**2)/freqRatio)
-win = np.ones(N)
-win *= 1.0/N
-filtered = scipy.signal.lfilter(win, [1], data)
+# Se genera el grafico
+plotFunction.figure('Frecuencia audio original')
+plotFunction.title('Dominio de la frecuencia audio original')
+plotFunction.plot(freqs, fourierTransform)
 
+# Se genera un filtro paso bajo
+b, a = signal.butter(3, 0.05)
+
+# Se aplica el filtro al audio
+filtered = scipy.signal.lfilter(b, a, data)
+
+# Se calcula la transformada de fourier del audio filtrado
 fourierTransformFiltered = fourier(filtered)
 
-plotFunction.figure('Grafico 1')
-plotFunction.plot(freqs, fourierTransform)
-plotFunction.figure('Grafico 2')
+# Se grafica el dominio de la frecuencia
+plotFunction.figure('Dominio de la frecuencia Paso Bajo')
+plotFunction.title('Dominio de la frecuencia Paso Bajo')
 plotFunction.plot(freqs, fourierTransformFiltered)
 
-inverseFourierTransform = np.asarray(filtered, dtype=np.int16)
+# Se obtiene la transformada inversa
+inverseFourierTransform = inverseFourier(fourierTransformFiltered)
 
-wav.write('filtered.wav', freqs, inverseFourierTransform)
+plotFunction.figure('Dominio del tiempo Paso Bajo')
+plotFunction.title('Dominio del tiempo Paso Bajo')
+plotFunction.plot(audioTime, inverseFourierTransform)
 
-#plotFunction.plot(filtered)
+(freqLowPass, timeLowPass, audioSpectogramLowPass) = signal.spectrogram(filtered, fs)
+
+plotFunction.figure('Espectograma Paso Bajo')
+plotFunction.pcolormesh(timeLowPass, freqLowPass, np.log10(audioSpectogramLowPass))
+plotFunction.title("Espectograma Filtro Paso Bajo")
+plotFunction.xlabel('Tiempo (s)')
+plotFunction.ylabel('Frecuencia (Hz)')
+plotFunction.colorbar()
+
+# Se genera archivo de audio lowpass
+finalSignal = np.asarray(inverseFourierTransform, dtype=np.int16)
+wav.write('filtrado.wav', fs, finalSignal)
 
 #Se muestran los graficos
 plotFunction.show()
